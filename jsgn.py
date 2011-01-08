4b1ebf924d64624aa42e26cd44e52999fdb0df35
Jeff Hammel <jhammel@system76-pc.(none)>
2011-01-07 21:55:52 -0800

    iterate over edges

--- jsgn.py
+++ jsgn.py
@@ -5,18 +5,16 @@ import os
 class DirectedGraph(object):
 
     def __init__(self, filename):
+        self.nodes = {}
+        self.edges = {}
         if filename and os.path.isfile(filename):
             f = open(filename,"r")
             self.graph = json.loads(f.read())
-        else:
-            self.graph = {'nodes': {}, 'edges': {}}
         self.filename = filename
 
-        # conveniences
-        self.nodes = self.graph['nodes']
-        self.edges = self.graph['edges']
-
     def dump(self):
+        edges
+        
         return json.dumps(self.graph)
         
     def save(self):
@@ -33,6 +31,11 @@ class DirectedGraph(object):
         self.add_node(node1)
         self.add_node(node2)
 
+    def __iter__(self):
+        for node1, value in self.edges.items():
+            for node2, metadata in value.items():
+                yield node1, node2, metadata
+
 if __name__ == '__main__':
     graph = DirectedGraph(None)
     graph.add_edge('foo', 'bar', **{'count': 2})
@@ -44,3 +47,10 @@ if __name__ == '__main__':
 
     count = sum([sum([j.get('count', 0) for j in i.values()]) for i in graph.edges.values()])
     assert count == 9
+
+    # iterate over edges
+    for node1, node2, metadata in graph:
+        print '%s -> %s : %s' % (node1, node2, metadata)
+
+    assert sum([j[2].get('count', 0) for j in graph]) == 9
+
