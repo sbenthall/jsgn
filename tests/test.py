a3d8216f02299f31b795457f69540aa57af0eb25
Jeff Hammel <jhammel@mozilla.com>
2011-01-09 11:21:57 -0800

    stub some tests

---
+++ tests/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+doctest runner
+"""
+
+import doctest
+import os
+
+def run_tests():
+    directory = os.path.dirname(os.path.abspath(__file__))
+    extraglobs = {'here': directory}
+    tests =  [ test for test in os.listdir(directory)
+               if test.endswith('.txt') ]
+    for test in tests:
+        doctest.testfile(test, extraglobs=extraglobs, raise_on_error=True)
+
+if __name__ == '__main__':
+    run_tests()
+
